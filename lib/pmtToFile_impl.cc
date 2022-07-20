2fe53cd9d1b00847edb99e5234967140b954615f
Avery <avery.moates17@ou.edu>
2022-07-20 11:12:19 -0500

    Added the turn off file write.

--- lib/pmtToFile_impl.cc
+++ lib/pmtToFile_impl.cc
@@ -14,21 +14,22 @@ namespace gr {
 namespace adsbTestBlocks {
 
 
-pmtToFile::sptr pmtToFile::make(std::string& filename)
+pmtToFile::sptr pmtToFile::make(std::string& filename, bool test)
 {
-    return gnuradio::make_block_sptr<pmtToFile_impl>(filename);
+    return gnuradio::make_block_sptr<pmtToFile_impl>(filename, test);
 }
 
 
 /*
  * The private constructor
  */
-pmtToFile_impl::pmtToFile_impl(std::string& filename)
+pmtToFile_impl::pmtToFile_impl(std::string& filename, bool test)
     : gr::block("pmtToFile",
                 gr::io_signature::make(0, 0, 0),
                 gr::io_signature::make(0, 0, 0)),
                 _filename(filename)
 {
+    _test = test;
     message_port_register_in(pmt::mp("Input"));
     set_msg_handler(pmt::mp("Input"),[this](const pmt::pmt_t& msg){this->writeToFile(msg);});
 
@@ -45,7 +46,7 @@ pmtToFile_impl::~pmtToFile_impl() {
 void pmtToFile_impl::writeToFile(const pmt::pmt_t& msg){
     std::string info = pmt::symbol_to_string(msg);
 
-    if(!_file.is_open()){
+    if(!_file.is_open() || !_test){
 
     }
     else{
